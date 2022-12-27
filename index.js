a3d34c702f8508b11ea84898b8dc998e2892f60c
joedonovan <joepeterdonovan@gmail.com>
2022-12-27 10:43:09 -0600

    updated to make closeModal function work

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const modal = document.getElementById('simpleModal');
 //get open modal button 
 const modalBtn = document.getElementById('modalBtn'); 
 // get close button 
-const closeBtn = document.getElementById('closeBtn'); 
+const closeBtn = document.getElementsByClassName('closeBtn')[0]; 
 
-// listen for click 
+// listen for open click 
 
 modalBtn.addEventListener('click', openModal); 
 
 // function to open modal 
 function openModal(){
     modal.style.display = 'block'; 
+}
+
+// listen for close click
+closeBtn.addEventListener('click', closeModal); 
+
+// function to close modal 
+
+//
+function closeModal(){
+    modal.style.display = 'none'; 
 }
